cadc39540993dc530fc73f663c1c056ec7495ad0
memememomo <memememomo@gmail.com>
2013-05-10 02:03:10 +0900

    add pod

--- lib/Karasu.pm
+++ lib/Karasu.pm
@@ -487,29 +487,360 @@ sub DESTROY {
 
 
 1;
-__END__
 
+__END__
 =head1 NAME
 
-Karasu - very simple DBI/SQL::Maker/DBIx::TransactionManager wrapper
+Karasu - very simple DBI wrapper
 
 =head1 SYNOPSIS
 
-  use Karasu;
+    my $db = MyDB->new({ connect_info => [ 'dbi:SQLite:' ] });
+    my $id = $db->insert( 'table' => {
+        col1 => $value
+    } );
 
 =head1 DESCRIPTION
 
-Karasu is
+Karasu is very simple DBI wrapper.
+It aims to be lightweight, with minimal dependencies so it's easier to install.
 
-=head1 AUTHOR
+=head1 BASIC USAGE
 
-memememomo E<lt>memememomo {at} gmail.comE<gt>
+create your db model base class.
+
+    package Your::Model;
+    use parent 'Karasu';
+    1;
+
+    use Your::Model;
+
+    my $karasu = Your::Model->new(\%args);
+    # insert new record.
+    my $id = $karasu->insert('user',
+        {
+            id   => 1,
+        }
+    );
+    $karasu->update('user', {name => 'memememomo'}, {id => $id});
+
+    $row = $karasu->single_by_sql(q{SELECT id, name FROM user WHERE id = ?}, [ 1 ]);
+    $karasu->delete('user', {id => $row->{id}});
+
+=head1 ARCHITECTURE
+
+Karasu classes are comprised of one distinct component:
+
+=head2 MODEL
+
+The C<model> is where you say
+
+    package MyApp::Model;
+    use parent 'Karasu';
+
+This is the entry point to using Karasu. You connect, insert, update, delete, select stuff using this object.
+
+
+=head1 METHODS
+
+Karasu provides a number of methods to all your classes,
+
+=over
+
+=item $karasu = Karasu->new(\%args)
+
+Creates a new Karasu instance.
+
+    # connect new database connection.
+    my $db = Your::Model->new(
+        connect_info => [ $dsn, $username, $password, \%connect_options ]
+    );
+
+Arguments can be:
+
+=over
+
+=item * C<connect_info>
+
+Specifies the information required to connect to the database.
+The argument should be a reference to a array in the form:
+
+    [ $dsn, $user, $password, \%options ]
+
+You must pass C<connect_info> or C<dbh> to the constructor.
+
+=item * C<dbh>
+
+Specifies the database handle to use.
+
+=item * C<fields_case>
+
+specific DBI.pm's FetchHashKeyName.
+
+=item * C<sql_builder>
+
+Speficies the SQL builder object. By default SQL::Maker is used, and as such,
+if you provide your own SQL builder the interface needs to be compatible
+with SQL::Maker.
+
+=back
+
+=item $id = $karasu->insert($table_name, \%row_data)
+
+Inserts a new record. Returns the primary key.
+
+    my $id = $karasu->insert('user',{
+        id   => 1,
+        name => 'uchico',
+    });
+
+
+=item $karasu->bulk_insert($table_name, \@rows_data)
+
+Accepts either an arrayref of hashrefs.
+each hashref should be a structure suitable
+forsubmitting to a Your::Model->insert(...) method.
+
+insert many record by bulk.
+
+example:
+
+    Your::Model->bulk_insert('user',[
+        {
+            id   => 1,
+            name => 'nyaruko',
+        },
+        {
+            id   => 2,
+            name => 'kuuko',
+        },
+        {
+            id   => 3,
+            name => 'hasuta',
+        },
+    ]);
+
+=item $update_row_count = $karasu->update($table_name, \%update_row_data, [\%update_condition])
+
+Calls UPDATE on C<$table_name>, with values specified in C<%update_ro_data>, and returns the number of rows updated. You may optionally specify C<%update_condition> to create a conditional update query.
+
+    my $update_row_count = $karasu->update('user',
+        {
+            name => 'uchico',
+        },
+        {
+            id => 1
+        }
+    );
+    # Executes UPDATE user SET name = 'uchico' WHERE id = 1
+
+
+=item $delete_row_count = $karasu->delete($table, \%delete_condition)
+
+Deletes the specified record(s) from C<$table> and returns the number of rows deleted. You may optionally specify C<%delete_condition> to create a conditional delete query.
+
+    my $rows_deleted = $karasu->delete( 'user', {
+        id => 1
+    } );
+    # Executes DELETE FROM user WHERE id = 1
+
+
+=item $itr = $karasu->search($table_name, [\%search_condition, [\%search_attr]])
+
+simple search method.
+search method get Karasu::Iterator's instance object.
+
+see L<Karasu::Iterator>
+
+get iterator:
+
+    my $itr = $karasu->search('user',{id => 1},{order_by => 'id'});
+
+get rows:
+
+    my @rows = $karasu->search('user',{id => 1},{order_by => 'id'});
+
+=item $row = $karasu->single($table_name, \%search_condition)
+
+get one record.
+give back one case of the beginning when it is acquired plural records by single method.
+
+    my $row = $karasu->single('user',{id =>1});
+
+=item $itr = $karasu->search_named($sql, [\%bind_values, [$table_name]])
+
+execute named query
+
+    my $itr = $karasu->search_named(q{SELECT * FROM user WHERE id = :id}, {id => 1});
+
+If you give ArrayRef to value, that is expanded to "(?,?,?,?)" in SQL.
+It's useful in case use IN statement.
+
+    # SELECT * FROM user WHERE id IN (?,?,?);
+    # bind [1,2,3]
+    my $itr = $karasu->search_named(q{SELECT * FROM user WHERE id IN :ids}, {ids => [1, 2, 3]});
+
+=item $itr = $karasu->search_by_sql($sql, [\@bind_values])
+
+execute your SQL
+
+    my $itr = $karasu->search_by_sql(q{
+        SELECT
+            id, name
+        FROM
+            user
+        WHERE
+            id = ?
+    },[ 1 ]);
+
+
+=item $row = $karasu->single_by_sql($sql, [\@bind_values, [$table_name]])
+
+get one record from your SQL.
+
+    my $row = $karasu->single_by_sql(q{SELECT id,name FROM user WHERE id = ? LIMIT 1}, [1]);
+
+This is a shortcut for
+
+    my $row = $karasu->search_by_sql(q{SELECT id,name FROM user WHERE id = ? LIMIT 1}, [1])->next;
+
+But optimized implementation.
+
+=item $row = $karasu->single_named($sql, [\%bind_values])
+
+get one record from execute named query
+
+    my $row = $karasu->single_named(q{SELECT id,name FROM user WHERE id = :id LIMIT 1}, {id => 1}, 'user');
+
+This is a shortcut for
+
+    my $row = $karasu->search_named(q{SELECT id,name FROM user WHERE id = :id LIMIT 1}, {id => 1})->next;
+
+But optimized implementation.
+
+=item $sth = $karasu->execute($sql, [\@bind_values])
+
+execute query and get statement handler.
+and will be inserted caller's file and line as a comment in the SQL if $ENV{KARASU_SQL_COMMENT} or sql_comment is true value.
+
+=item $karasu->txn_scope
+
+Creates a new transaction scope guard object.
+
+    do {
+        my $txn = $karasu->txn_scope;
+
+        $karasu->update('user', {foo => 'bar'});
+
+        $txn->commit;
+    }
+
+If an exception occurs, or the guard object otherwise leaves the scope
+before C<< $txn->commit >> is called, the transaction will be rolled
+back by an explicit L</txn_rollback> call. In essence this is akin to
+using a L</txn_begin>/L</txn_commit> pair, without having to worry
+about calling L</txn_rollback> at the right places. Note that since there
+is no defined code closure, there will be no retries and other magic upon
+database disconnection.
+
+=item $txn_manager = $karasu->txn_manager
+
+Get the DBIx::TransactionManager instance.
+
+=item $karasu->txn_begin
+
+start new transaction.
+
+=item $karasu->txn_commit
+
+commit transaction.
+
+=item $karasu->txn_rollback
+
+rollback transaction.
+
+=item $karasu->txn_end
+
+finish transaction.
+
+=item $karasu->do($sql, [\%option, @bind_values])
+
+Execute the query specified by C<$sql>, using C<%option> and C<@bind_values> as necessary. This pretty much a wrapper around L<http://search.cpan.org/dist/DBI/DBI.pm#do>
+
+=item $karasu->dbh
+
+get database handle.
+
+=item $karasu->connect(\@connect_info)
+
+connect database handle.
+
+connect_info is [$dsn, $user, $password, $options].
+
+If you give \@connect_info, create new database connection.
+
+=item $karasu->disconnect()
+
+Disconnects from the currently connected database.
+
+=item $karasu->load_plugin();
+
+ $karasu->load_plugin($plugin_class, $options);
+
+This imports plugin class's methods to C<$karasu> class
+and it calls $plugin_class's init method if it has.
+
+ $plugin_class->init($karasu, $options);
+
+If you want to change imported method name, use C<alias> option.
+for example:
+
+ YourDB->load_plugin('BulkInsert', { alias => { bulk_insert => 'isnert_bulk' } });
+
+BulkInsert's "bulk_insert" method is imported as "insert_bulk".
+
+=item $karasu->handle_error
+
+handling error method.
+
+=item How do you use display the profiling result?
+
+use L<Devel::KYTProf>.
+
+=back
+
+=head1 TRIGGERS
+
+Karasu does not support triggers (NOTE: do not confuse it with SQL triggers - we're talking about Perl level triggers). If you really want to hook into the various methods, use something like L<Moose>, L<Mouse>, and L<Class::Method::Modifiers>.
+
+=head1 Row Object, Inflate/Deflate, Table Schema
+
+Karasu does not support Row Object, Inflate/Deflate, Table Schema. If you really want to use them, use L<Teng>.
 
 =head1 SEE ALSO
 
-=head1 LICENSE
+=head2 Fork
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This module was forked from L<Teng>, around version 0.18.
+many incompatible changes have been made.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+=head1 AUTHORS
+
+Uchico  C<< <memememomo __at__ gmail.com> >>
+
+=head1 REPOSITORY
+
+  git clone https://github.com/memememomo/p5-Karasu
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2010, the Karasu L</AUTHOR>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
 
 =cut
